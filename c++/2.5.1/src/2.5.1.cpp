1a440b8bf2e3240d8ab640d7aae77c51787855a6
JG <jgoodrich0315@vbtc.net>
2018-10-19 09:55:31 -0400

    new stuff

--- c++/2.5.1/src/2.5.1.cpp
+++ c++/2.5.1/src/2.5.1.cpp
@@ -10,24 +10,11 @@
 using namespace std;
 
 int main() {
-	int above0,c0,step=0;
-	cout<<"give me a number above zero: (no decimals)";
-	cin>>above0;
-	if(above0>=1){
-		c0=above0;
-		do{
-			if(c0 % 2==0){
-				c0=c0/2;
-				cout<<c0<<endl;
-			}
-			else{
-				c0=c0*3+1;
-				cout<<c0<<endl;
-			}
-			step++;
-		}while(c0!=1);
-		cout<<"steps"<<" = "<<step<<endl;
-	}
+	int Pick,A,B,C,D;
+	cout << "[calculator]"<< endl;
+	cout << "0 - exit" << endl << "1 - addition" << endl << "2 - subtraction " << endl << "3 - multiplication"<< endl << "4 - division"<< endl << "Pick one you git";
+cin >> Pick;
+If
 
 
 
@@ -40,12 +27,6 @@ int main() {
 
 
 
-
-	if(above0<1)
-	{
-		//cout<<"you git,you entered a number that isnt above zero";
-
-	}
 
 
 	return 0;
