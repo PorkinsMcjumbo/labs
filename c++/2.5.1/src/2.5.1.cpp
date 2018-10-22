ed6b1a7eb7bdc731c27262a628a959935a4b66ab
JG <jgoodrich0315@vbtc.net>
2018-10-22 09:53:58 -0400

    eeee

--- c++/2.5.1/src/2.5.1.cpp
+++ c++/2.5.1/src/2.5.1.cpp
@@ -10,20 +10,49 @@
 using namespace std;
 
 int main() {
-	int Pick,A,B,C,D;
+	float A,B,C;
+char Pick;
 	cout << "[calculator]"<< endl;
-	cout << "0 - exit" << endl << "1 - addition" << endl << "2 - subtraction " << endl << "3 - multiplication"<< endl << "4 - division"<< endl << "Pick one you git";
+	cout << "0 - exit" << endl << "1 - addition" << endl << "2 - subtraction " << endl << "3 - multiplication"<< endl << "4 - division"<< endl << "Pick one you git"<< endl;
 cin >> Pick;
-If
 
 
+switch(Pick){
+case '1' :
+cout<<"enter two number to add"<<endl;
+cin >> A;
+cin >> B;
+C=A+B;
+cout << A << "+" << B << "=" << C << endl;
 
+break;
 
+case'2' :
+cout << "enter two numbers to subtract"<<endl;
+cin >> A;
+cin >> B;
+C=A-B;
+cout << A <<"-" << B << "=" << C << endl;
 
+break;
 
+case'3' :
+cout<<"enter two number to multiply"<<endl;
+cin>>A;
+cin>>B;
+C=A*B;
+cout << A <<" X " << B << " = " << C << endl;
 
+break;
+case'4' :
+cout<<"enter two number to divide"<<endl;
+cin>>A;
+cin>>B;
+C=A/B;
+cout << A <<" / " << B << " = " << C << endl;
 
-
+break;
+}
 
 
 
