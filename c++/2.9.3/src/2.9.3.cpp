87d18cdc982d99fec078f435746082a7e6564ec8
JG <jgoodrich0315@vbtc.net>
2018-10-29 10:00:57 -0400

    eee

--- c++/2.9.3/src/2.9.3.cpp
+++ c++/2.9.3/src/2.9.3.cpp
@@ -10,33 +10,31 @@
 using namespace std;
 
 int main() {
-int vector[7] = {1,7,3,8,3,7,1};
-bool ispalindrome = true;
-int n= sizeof(vector) / sizeof(vector[0]);
+	int a=0,b;
+	int vector[7] = {1,7,3,8,3,7,1};
+	bool ispalindrome = true;
+	int n= sizeof(vector) / sizeof(vector[0]);
+	b=n-1;
+	for(int i=0;i<n/2;i++){
+		if(vector[a]==vector[b]){
+			a++;
+			b--;
+		}//if1
+		else
+		{
+			ispalindrome=false;
+			break;
+		}//else1
 
+	}//loop
+	if(ispalindrome){
+		cout<<" it is a palindrome "<<endl;
+	}// if2
 
-if(ispalindrome){
-cout<<" it is a palindrome "<<endl;
-}// if
+	else
+	{
+		cout<<"It isn't a palindrome"<< endl;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
-}
+	}// else2
+	return 0;
+}// main
